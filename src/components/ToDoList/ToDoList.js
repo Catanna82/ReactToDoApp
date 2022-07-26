a44b5715bbbdb495d739c63b67dd11d6aa21dee2
Anna Todorova <anna820614@gmail.com>
2022-07-26 14:39:31 +0300

    Add empty row

--- src/components/ToDoList/ToDoList.js
+++ src/components/ToDoList/ToDoList.js
@@ -1,8 +1,5 @@
-<<<<<<< HEAD
 import React from 'react';
 import ToDo from '../Todo/ToDo';
-
-
 
 const ToDoList = ({todos, setTodos, filteredTodos}) => {
     return (
@@ -25,31 +22,3 @@ const ToDoList = ({todos, setTodos, filteredTodos}) => {
 }
 
 export default ToDoList;
-=======
-import React from 'react';
-import ToDo from '../Todo/ToDo';
-
-
-
-const ToDoList = ({todos, setTodos, filteredTodos}) => {
-    return (
-        <div className='todo-container'>
-            <ul className='todo-list'>
-                {filteredTodos.map((todo) => (
-                    <ToDo 
-                    setTodos={setTodos}
-                    todos={todos} 
-                    text={todo.text} 
-                    key={todo.id}
-                    todo={todo}/>
-                ))}
-            </ul>
-            <div className='img-container'>
-                <img src='../images/toDoList.png' alt='pic'/>
-            </div>
-        </div>
-    );
-}
-
-export default ToDoList;
->>>>>>> 1ef3ebd37c4fdcdd60b838ae79dbfbbcf5eef5b2
